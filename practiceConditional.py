78c7851f5b42ba10a5cf6600cb6d00cf33455251
gsanchez2 <58314861+gsanchez2@users.noreply.github.com>
2020-02-15 11:54:10 -0600

    Initial commit

---
+++ practiceConditional.py
@@ -0,0 +1,25 @@
+'''
+Created on Feb 12, 2020
+
+@author: ITAUser
+'''
+x = 2
+
+x = 3
+
+if(x==1):
+    print("something")
+    
+if(x==0):
+    print("some")
+elif(x==1):
+    print("thing")
+elif(x==2):
+    print(5) 
+elif(x==3):
+    print(3)
+else:
+    print("N/A")
+    
+
+    

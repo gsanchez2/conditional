7f59d37aa2ff89ed4c181c3c484d714c113b7178
gsanchez2 <58314861+gsanchez2@users.noreply.github.com>
2020-02-15 12:53:44 -0600

    Create debugConditionalsV1.py

--- conditionalDrills.py
+++ conditionalDrills.py
@@ -5,15 +5,30 @@ For this assignment you should read the task, then below the task do what it ask
 '''
 #1) Create a Variable called grade and set it to an integer. Make an if statement that checks if the grade is a passing grade. grade must be above 65 to pass. print out "student is passing"
 '''
+grade = 82
 
+if(grade>65):
+    print("passing")
 '''
 #2) Now make an if, else statement that checks if the student is passing but also print "student is failing", if the grade is less than 65
 '''
-
+if(grade<65):
+    print("student is failing")
+else:
+    print("passing")
 '''
 #3)Create a variable called age. Make and if, else statement that checks if the age entered is old enough to vote. Remember the voting age is 18
 '''
+age = 16
 
+if(age>18):
+    print("able to vote")
+    
+if(age<18):
+    print("not able to vote")
+else:
+    print("able to vote")
+    
 '''
 #4)Create a variable called weight. Make an if statement that checks the unit of the weight. If the weight is in kilograms, convert it to pounds 
 '''
